ec0881a766be9b000ed3f1205e206f4fe3158681
MyNa Do <myna@mynado.se>
2025-10-22 13:26:29 +0200

    fix: change copy

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -7,11 +7,12 @@ export default function Projects() {
     <div className="p-4 max-w-5xl mx-auto">
       <h1 className="text-5xl md:text-7xl font-extrabold mb-8">Projects</h1>
       <p className="mb-8">
-        Core Expertise: My work, developed through independent and freelance
-        initiatives, demonstrates full-cycle product ownership and
-        performance-driven architecture. I specialize in modern decoupled
-        stacks, managing complex monorepos with Sanity.io and Vite, and
-        achieving high-speed delivery via Prerendering (SSG).
+        Core Expertise: Backed by five years of professional experience in the
+        industry, these selected independent and freelance projects demonstrate
+        my capability in full-cycle product ownership and performance-driven
+        architecture. I specialize in modern decoupled stacks, managing complex
+        monorepos with Sanity.io and Vite, and achieving high-speed delivery via
+        Prerendering (SSG).
       </p>
       <section>
         <div className="flex flex-wrap gap-4 justify-center items-stretch mx-auto w-full">
