343bcb1acdb9848ded093bb3a3b4ea3bcb3d0214
MyNa Do <myna@mynado.se>
2025-10-23 10:19:31 +0200

    fix: touch area

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,22 +2,24 @@ import { GitHubIcon, LinkedInIcon } from "assets/index";
 
 export default function Footer() {
   return (
-    <div className="py-8 px-4 text-center">
+    <div className="p-4 text-center">
       <ul className="list-none p-0 m-0">
         <li className="flex justify-center gap-2 font-semibold">
           <a href="https://github.com/mynado" target="_blank" rel="noreferrer">
-            <img className="w-4" src={GitHubIcon} alt="GitHub" />
+            <img src={GitHubIcon} alt="GitHub" width={24} height={24} />
           </a>
           <a
             href="https://www.linkedin.com/in/mynado/"
             target="_blank"
             rel="noreferrer"
           >
-            <img className="w-4" src={LinkedInIcon} alt="LinkedIn" />
+            <img src={LinkedInIcon} alt="LinkedIn" width={24} height={24} />
           </a>
         </li>
         <li className="flex justify-center gap-2 font-semibold">
-          <a href="mailto: myna@mynado.se">myna@mynado.se</a>
+          <a href="mailto: myna@mynado.se" className="h-full py-2 px-4">
+            myna@mynado.se
+          </a>
         </li>
       </ul>
     </div>
